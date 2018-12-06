515aacdf57c91d94c9617f6c8d7ca80ec4861272
Madison Abegglen <madison.abegglen@gmail.com>
2018-12-06 13:38:23 -0700

    tasks work

--- server/server-assets/routes/task-routes.js
+++ server/server-assets/routes/task-routes.js
@@ -2,15 +2,95 @@ const router = require('express').Router()
 const Tasks = require('../models/task')
 
 // GET
-
+router.get('/:listId', (req, res, next) => {
+  Tasks.find({ listId: req.params.listId })
+    .then(tasks => {
+      res.send(tasks)
+    })
+    .catch(error => {
+      console.log('[GET TASKS ERROR]', error)
+      next(error)
+    })
+})
 
 // POST
-
+router.post('/', (req, res, next) => {
+  Tasks.create(req.body)
+    .then(task => res.send(task))
+    .catch(error => {
+      console.log('[POST TASK ERROR]', error)
+      next(error)
+    })
+})
 
 // PUT
-
+router.put('/:id', (req, res, next) => {
+  Tasks.findByIdAndUpdate(req.params.id, req.body)
+    .then(task => {
+      res.send({ message: 'Successfully updated task: ', task })
+    })
+    .catch(error => {
+      console.log('[PUT TASK ERROR]', error)
+      next(error)
+    })
+})
 
 // DELETE
+router.delete('/:id', (req, res, next) => {
+  Tasks.findByIdAndRemove(req.params.id)
+    .then(() => res.send({ message: 'Successfully DELORTED' }))
+    .catch(error => {
+      console.log('[DELETE TASK ERROR]', error)
+      next(error)
+    })
+})
 
+// ----- COMMENTS REQ'S -----
+// GET
+router.get('/comments/:taskId', (req, res, next) => {
+  Tasks.find({ taskId: req.params.taskId })
+    .then(task => res.send(task.comments))
+    .catch(error => {
+      console.log('[GET TASK COMMENTS ERROR]', error)
+      next(error)
+    })
+})
+
+// POST
+router.post('/comments/:taskId', (req, res, next) => {
+  Tasks.findById(req.params.taskId)
+    .then(task => {
+      task.comments = task.comments.concat(req.body)
+      const comment = task.comments[task.comments.length - 1]
+      task.save(error => {
+        if (error) {
+          return next(error)
+        }
+        res.send(comment)
+      })
+    })
+    .catch(error => {
+      console.log('[POST TASK COMMENT ERROR]', error)
+      next(error)
+    })
+})
+
+// DELETE
+router.delete('/comments/:taskId/:commentId', (req, res, next) => {
+  Tasks.findById(req.params.taskId)
+    .then(task => {
+      task.comments = task.comments.filter(comment => comment._id !== commentId)
+      task.save(error => {
+        if (error) {
+          return next(error)
+        }
+        res.send({ message: 'Successfully DELORTED comment' })
+      })
+    })
+    .catch(error => {
+      console.log('[DELETE TASK COMMENT ERROR]', error)
+      next(error)
+    })
+})
 
 module.exports = router
