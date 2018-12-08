5f61bd44a1b6d2d913718dd494d69a6fd518f6a2
Madison Abegglen <madison.abegglen@gmail.com>
2018-12-07 20:44:46 -0700

    the form looks kinda bettter

--- client/src/components/RegLogForm.vue
+++ client/src/components/RegLogForm.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="col-6 offset-3 form-p">
-    <h1>This is the form</h1>
-    <form @submit.prevent></form>
+    <form @submit.prevent class="form">
+      <div class="form-group" v-if="!isLogin">
+        <label for="name">
+          <i class="fas fa-user-circle"></i>
+        </label>
+        <input type="text" name="name" class="input" v-model="name">
+      </div>
+      <div class="form-group">
+        <label for="email">
+          <i class="fas fa-at"></i>
+        </label>
+        <input type="text" name="email" class="input" v-model="email">
+      </div>
+      <div class="form-group">
+        <label for="password">
+          <i class="fas fa-lock"></i>
+        </label>
+        <input type="text" name="password" class="input" v-model="password">
+      </div>
+      <button type="submit" class="btn btn-info">{{ isLogin ? 'Login' : 'Register'}}</button>
+      <button type="button" class="btn btn-info">Switch to {{isLogin ? 'Signup' : 'Login'}}</button>
+    </form>
   </div>
 </template>
 
@@ -27,5 +47,24 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+}
+.form {
+  height: 40vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: rgb(117, 117, 117, 0.5);
+}
+.form-group {
+  display: flex;
+  flex-direction: row;
+}
+.btn-info {
+  border-radius: 0;
+  margin: 3px;
+  width: 60%;
 }
 </style>
