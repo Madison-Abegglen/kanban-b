aab7594b2bd1589975e52d8915fb370a57ba48dc
Madison Abegglen <madison.abegglen@gmail.com>
2018-12-10 11:19:21 -0700

    navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row navbar">
+    <div class="col-8 nav-title">
+      <h1 class="txt-2">
+        K
+        <i class="fas fa-angle-double-up" style="padding-right: 8px;"></i>nban.
+      </h1>
+    </div>
+    <div class="col-4 nav-options">
+      <h6 class="txt-2">
+        <i class="fas fa-user-circle" style="padding-right: 5px;"></i>logout
+      </h6>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "navbar",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped>
+.navbar {
+  height: 10vh;
+  top: 0;
+  min-width: 100%;
+  background-color: rgb(145, 43, 43);
+  color: rgb(245, 245, 245);
+  position: fixed;
+  z-index: 2;
+}
+.nav-options {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.txt-1 {
+  font-family: "Montserrat", sans-serif;
+  text-transform: uppercase;
+  /* letter-spacing: 4px; */
+  color: black;
+}
+.txt-2 {
+  font-family: "Montserrat", sans-serif;
+  text-transform: uppercase;
+  color: white;
+  font-weight: bold;
+}
+</style>
--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="boards">
-    <h1>This is the boards page</h1>
+  <div class="boards container-fluid">
+    <Navbar></Navbar>
+    <div class="row body">
+      <div class="col-12">
+        <h1>This is the boards page</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
 export default {
   name: "boards",
-  components: {},
   data() {
     return {};
+  },
+  components: {
+    Navbar
   },
   computed: {},
   methods: {}
@@ -17,4 +25,12 @@ export default {
 </script>
 
 <style scoped>
+.container-fluid {
+  height: 100vh;
+  overflow: hidden;
+  background-color: rgb(250, 250, 250, 0.8);
+}
+.body {
+  margin-top: 10vh;
+}
 </style>
